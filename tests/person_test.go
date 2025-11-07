ba897fd0dcd4e91e6e7dea4d627f6cb24d863689
Vlad Eryn <vlad2004e@gmail.com>
2025-11-07 16:37:08 +0300

    added test for create person

--- tests/person_test.go
+++ tests/person_test.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -32,7 +33,7 @@ func TestReadPersons(t *testing.T) {
 	storage.Pool.Exec(context.Background(), "TRUNCATE TABLE persons CASCADE")
 	storage.Pool.Exec(context.Background(), "INSERT INTO persons (name, lastName) VALUES ('Mike', 'Black')")
 
-	req := httptest.NewRequest("GET", "/persons", nil)
+	req := httptest.NewRequest(http.MethodGet, "/persons", nil)
 	w := httptest.NewRecorder()
 
 	service.GetPersons(w, req)
@@ -56,6 +57,52 @@ func TestReadPersons(t *testing.T) {
 
 	if !isExist {
 		t.Fatalf("Mike dont exists in DB")
+	}
+
+}
+
+func TestCreatePerson(t *testing.T) {
+
+	ctx := context.Background()
+
+	config, err := pgxpool.ParseConfig("postgres://postgres:2004@localhost:5432/test_crud_go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	config.MaxConns = 5
+	config.MinConns = 1
+
+	storage.Pool, err = pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	storage.Pool.Exec(context.Background(), "TRUNCATE TABLE persons CASCADE")
+
+	values := strings.NewReader("{\"name\": \"Mike\", \"lastName\": \"Black\"}")
+	req := httptest.NewRequest(http.MethodPost, "/persons", values)
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+	service.CreatePerson(w, req)
+
+	if w.Code != http.StatusCreated {
+		t.Fatalf("waiting 201, get:  %d", w.Code)
+	}
+
+	var person models.Person
+	if err := json.NewDecoder(w.Body).Decode(&person); err != nil {
+		t.Fatalf("wrong JSON: %s", err)
+	}
+
+	if person.Name != "Mike" {
+		t.Fatalf("Waiting Mike, get: %s", person.Name)
+	}
+
+	var count int
+	storage.Pool.QueryRow(ctx, "SELECT COUNT(*) FROM persons WHERE name = $1", "Mike").Scan(&count)
+	if count != 1 {
+		t.Fatal("Person not saved in db")
 	}
 
 }
